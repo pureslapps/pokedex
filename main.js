e27bd6b2536ae4f8aa1b9b8fba1a6edc9968586a
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-22 16:55:05 -0400

    final commit at yearup

--- main.js
+++ main.js
@@ -1,81 +1,26 @@
-// .bg {
-//   animation: kenburns 20s infinite;
-// }
-// @keyframes kenburns {
-//   0% {
-//     opacity: 0;
-//   }
-//   5% {
-//     opacity: 1;
-//   }
-//   95% {
-//     transform: scale3d(1.5, 1.5, 1.5) translate3d(-190px, -120px, 0px);
-//     animation-timing-function: ease-in;
-//     opacity: 1;
-//   }
-//   100% {
-//     transform: scale3d(2, 2, 2) translate3d(-170px, -100px, 0px);
-//     opacity: 0;
-//   }
-// }
-
-// const BASEURL = 'https://www.pokemon.com/'
-//
-// requestAPI = () => {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//      data = JSON.parse(this.responseText)
-//      for (item in data)
-//       let pokemon = new Pokemon {
-//        name:data['name']
-//        hp:data['hp']
-//        attk:data['attk']
-//        def:data['def']
-//        abilities:data['abilities']
-//      }
-//     }
-//   };
-//   xhttp.open("GET", "https://www.pokemon.com/us/pokedex/", true);
-//   xhttp.send();
-// }
-//
-// requestAPI()
-//
-// class pokemon = {
-//   constructor (name, hp, attk, def, abilities)
-//   this.name = name
-//   this.hp = hp
-//   this.attk = attk
-//   this. def = def
-//   this.abilites = abilities
-// }
-
-
-
 function pokeDex() {
-  var a = prompt('Enter your pokemon');
+  var pokeSearch = prompt('Enter your pokemon');
   var xhttp = new XMLHttpRequest();
-  console.log(`${a}`)
+  console.log(`${pokeSearch}`)
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      let data = JSON.parse(this.responseText)
        pokemon = {
        name: data['name'],
-       // health: data['hp'],
-       // attack: data['attack'],
-       // defense: data ['defense'],
-       // abilities: data['abilities']
+       health: data['stats']['5']['base_stat'],
+       attack: data['stats']['4']['base_stat'],
+       defense: data ['stats']['3']['base_stat'],
+       abilities: data['abilities']['0']['ability']['name']
           }
         }
       }
-    if (isNaN(a)) {
-      xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${a.toLowerCase()}.json`, true);
+    if (isNaN(pokeSearch)) {
+      xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${pokeSearch.toLowerCase()}.json`, true);
     }else {
-      xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/id/${a}.json`, true);
+      xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/id/${pokeSearch}.json`, true);
     }
     xhttp.send();
-    console.log(`https://fizal.me/pokeapi/api/v2/id/${a}.json`)
+    console.log(`https://fizal.me/pokeapi/api/v2/id/${pokeSearch}.json`)
 };
 
 
@@ -90,7 +35,8 @@ class pokemon  {
   this.abilites = abilities
   }
 }
-displayPokemon = () =>{
+
+ function displayPokemon() {
   let cont = document.createElement('div')
   let h1 = document.createElement('h1')
   let p1 = document.createElement('p')
@@ -101,3 +47,5 @@ displayPokemon = () =>{
   cont.appendChild(p1)
   document.body.appendChild(cont)
 }
+
+displayPokemon()
